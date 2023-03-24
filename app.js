cbb873b6b5ed47f43dc432a79aaff3bd07820405
Fgonzalez409 <fergonz409@gmail.com>
2023-03-23 19:55:08 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const handleClick = () => {
+    let mainBody = document.getElementById("main-body");
+
+    if(mainBody.style.display === "none") {
+        mainBody.style.display ="block"
+    }
+
+    else{
+        mainBody.style.display = "none"
+    }
+
+    
+
+    // btn.addEventListener("click" , function(){
+    //     document.body.classList.toggle("no-main-body")
+    // });
+    
+}
