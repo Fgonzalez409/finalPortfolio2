8e4909c6da028641224ce31b60d52a3ad76e62cc
Fgonzalez409 <fergonz409@gmail.com>
2024-09-03 18:09:17 -0500

    added toggle text to home page

---
+++ toggleContent.js
@@ -0,0 +1,12 @@
+function toggleContent() {
+    var content = document.getElementById("additional-content");
+    var button = document.getElementById("learn-more-button");
+
+    if (content.style.display === "none" || content.style.display === "") {
+        content.style.display = "block";
+        button.innerHTML = '<i class="fa-solid fa-hand-point-left pulsating-icon"></i> See less';
+    } else {
+        content.style.display = "none";
+        button.innerHTML = '<i class="fa-solid fa-hand-point-right pulsating-icon"></i> Click here to learn more';
+    }
+}
