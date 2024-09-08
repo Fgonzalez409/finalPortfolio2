c72b08c040f8da29acccd686f6d7d87c08343a37
Fgonzalez409 <fergonz409@gmail.com>
2024-09-08 12:55:51 -0500

    edited the hand pointer on button

--- toggleContent.js
+++ toggleContent.js
@@ -4,7 +4,7 @@ function toggleContent() {
 
     if (content.style.display === "none" || content.style.display === "") {
         content.style.display = "block";
-        button.innerHTML = '<i class="fa-solid fa-hand-point-left pulsating-icon"></i> See less';
+        button.innerHTML = '<i class="fa-solid fa-hand-point-right pulsating-icon"></i> See less';
     } else {
         content.style.display = "none";
         button.innerHTML = '<i class="fa-solid fa-hand-point-right pulsating-icon"></i> Click here to learn more';
