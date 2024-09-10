078ee9c452989b45136399717425e4a098de3ba8
Fgonzalez409 <fergonz409@gmail.com>
2024-09-09 22:33:45 -0500

    reverting changes

--- toggleFeaturedProjects.js
+++ toggleFeaturedProjects.js
@@ -1,14 +1,14 @@
 function toggleFeaturedProjects() {
-    const button = document.getElementById('featured-projects-button');
-    const featuredProjects = document.getElementById('main-container');
+        const button = document.getElementById('featured-projects-button');
+        const featuredProjects = document.getElementById('main-container');
 
-    // Toggle visibility of featured projects
-    if (featuredProjects.classList.contains('content-hidden')) {
-        featuredProjects.classList.remove('content-hidden');
-    } else {
-        featuredProjects.classList.add('content-hidden');
+        // Toggle visibility of featured projects
+        if (featuredProjects.classList.contains('content-hidden')) {
+            featuredProjects.classList.remove('content-hidden');
+        } else {
+            featuredProjects.classList.add('content-hidden');
+        }
+
+        // Remove pulsating animation after button is clicked
+        button.classList.add('clicked');
     }
-
-    // Remove pulsating animation after button is clicked
-    button.classList.add('clicked');
-}
